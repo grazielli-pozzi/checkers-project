822b5f4232655deba4936762d522b54ea3e9e050
Mateus Exel <mateus@exel.com.br>
2019-04-10 16:43:37 -0300

    moves jumps visual, not working together

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ class piece {
    }
 } 
 
-
 function createBlackPieces() {
     let b1 = new piece('black', [1,0]);
     let b2 = new piece('black', [3,0]);
@@ -19,10 +18,9 @@ function createBlackPieces() {
     let b10 = new piece('black', [3,2]);
     let b11 = new piece('black', [5,2]);
     let b12 = new piece('black', [7,2]);
-    const blacks = [b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12];
-    return blacks
-
+    return [b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12];
 }
+
 function createRedPieces() {
     let r1 = new piece('red', [6,7]);
     let r2 = new piece('red', [4,7]);
@@ -36,224 +34,241 @@ function createRedPieces() {
     let r10 = new piece('red', [4,5]);
     let r11 = new piece('red', [2,5]);
     let r12 = new piece('red', [0,5]);
-    return reds=[r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12]
-}
-function updatedPositions(arr) {
-    let positions = [];
-    arr.forEach(element => {
-        positions.push(element.position);
-    });
-    return positions;
-}
-function allPositions(x, y){
-    return x.concat(y);
-       
+    return [r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12];
 }
 
-function moveWest(piece, allPositions){
+const redPieces = createRedPieces();
+const blackPieces = createBlackPieces();
+
+function placePieces(arrPieces) {
+    for (let i = 0; i < arrPieces.length; i += 1) {
+        let pieceSlot = document.querySelector(`#y-${arrPieces[i].position[1].toString()} > .x-${arrPieces[i].position[0].toString()}`);
+        if(arrPieces[i].team === 'red'){
+            pieceSlot.className += ' fire';
+        }
+        else {
+            pieceSlot.className += ' ice';
+
+        }
+    }
+}
+
+
+placePieces(blackPieces);
+placePieces(redPieces);
+console.log(blackPieces)
+showPossibilitiesBlack(blackPieces,redPieces);
+console.log(blackPieces)
+showPossibilitiesRed(blackPieces, redPieces);
+
+function showPossibilitiesBlack(blackPieces, redPieces){
+    for (let i = 0; i < blackPieces.length; i += 1){
+        document.querySelector(`#y-${blackPieces[i].position[1].toString()} > .x-${blackPieces[i].position[0].toString()}`).onclick = () => {
+            possibleWest = moveWest(blackPieces[i], blackPieces, redPieces);
+            possibleEast = moveEast(blackPieces[i], blackPieces, redPieces);
+            document.querySelector(`#y-${possibleWest[1].toString()} > .x-${possibleWest[0].toString()}`).onclick = () => { 
+                blackPieces[i].position = possibleWest;
+                console.log('kkk')
+                for(let y = 0; y < 2; y += 1){
+                    document.querySelector('.possible').classList.remove('possible')
+                }
+                for(let j = 0; j < blackPieces.length; j+=1){
+                    document.querySelector('.ice').classList.remove('ice')
+                }
+                placePieces(blackPieces)
+            }
+            document.querySelector(`#y-${possibleEast[1].toString()} > .x-${possibleEast[0].toString()}`).onclick = () => { 
+                blackPieces[i].position = possibleEast;
+                console.log('kkk')
+                for(let y = 0; y < 2; y += 1){
+                    document.querySelector('.possible').classList.remove('possible')
+                }
+                for(let j = 0; j < blackPieces.length; j+=1){
+                    document.querySelector('.ice').classList.remove('ice')
+                }
+                placePieces(blackPieces)
+            }
+        }
+    }
+}   
+    
+function showPossibilitiesRed(blackPieces, redPieces){
+    for (let i = 0; i < redPieces.length; i += 1){
+        document.querySelector(`#y-${redPieces[i].position[1].toString()} > .x-${redPieces[i].position[0].toString()}`).onclick = () => {
+            possibleWest = moveWest(redPieces[i], blackPieces, redPieces);
+            possibleEast = moveEast(redPieces[i], blackPieces, redPieces);
+            document.querySelector(`#y-${possibleWest[1].toString()} > .x-${possibleWest[0].toString()}`).onclick = () => { 
+                redPieces[i].position = possibleWest;
+                console.log('kkk')
+                for(let y = 0; y < 2; y += 1){
+                    document.querySelector('.possible').classList.remove('possible')
+                }
+                for(let j = 0; j < redPieces.length; j+=1){
+                    document.querySelector('.fire').classList.remove('fire')
+                }
+            placePieces(redPieces)
+            }
+            document.querySelector(`#y-${possibleEast[1].toString()} > .x-${possibleEast[0].toString()}`).onclick = () => { 
+                redPieces[i].position = possibleEast;
+                console.log('kkk')
+                for(let y = 0; y < 2; y += 1){
+                    document.querySelector('.possible').classList.remove('possible')
+                }
+                for(let j = 0; j < redPieces.length; j+=1){
+                    document.querySelector('.fire').classList.remove('fire')
+                }
+            placePieces(redPieces)
+            
+            }   
+        }
+    }
+}
+
+function moveWest(piece, blackPieces, redPieces){
     let answer = ''
-    let possible = []
+    let arrBlackPositions = [];
+    let arrRedPositions = [];
+    for (let b=0; b< blackPieces.length; b+=1){
+        arrBlackPositions += blackPieces[b].position
+    };
+    for (let r=0; r< redPieces.length; r+=1){
+        arrRedPositions += redPieces[r].position
+    }
+    arrAllPositions = arrBlackPositions.concat(arrRedPositions)
     if (piece.team === 'black'){
         possibleMoveWest = [piece.position[0]-1, piece.position[1]+1];
         if(possibleMoveWest[0]>7 || possibleMoveWest[0]<0 || possibleMoveWest[1] > 7 || possibleMoveWest[1]<0){
-            answer = "Impossible"
-            }
-        else{
-            for (let i=0; i< allPositions.length; i+=1){
-                if(allPositions[i][0] === possibleMoveWest[0] && allPositions[i][1] === possibleMoveWest[1]){
-                    answer = "Impossible"
-                    break;
-            }
-            else {
-                    answer = "possible"
-                }
-            }
-        }return answer
+        answer = 'impossible'
+        }
+        else if(arrAllPositions.indexOf(possibleMoveWest) === -1){
+            document.querySelector(`#y-${possibleMoveWest[1].toString()} > .x-${possibleMoveWest[0].toString()}`).classList+=' possible'
+            return possibleMoveWest
+        }
     }
-    else{
+    else {
         possibleMoveWest = [piece.position[0]-1, piece.position[1]-1]
-        for (let i=0; i< allPositions.length; i+=1){
-            if(possibleMoveWest[0]>7 || possibleMoveWest[0]<0|| possibleMoveWest[1] > 7 || possibleMoveWest[1]<0){
-                answer = "Impossible"
-                    break;
-                }
-            else if(allPositions[i][0] === possibleMoveWest[0] && allPositions[i][1] === possibleMoveWest[1]){
-                answer = "Impossible"
-                break;
-        }
-           else {
-                answer = "Possible"
-            }
-        }return answer
-    }
-}
-
-function moveEast(piece, allPositions){
-    let answer = ''
-    if (piece.team === 'black'){
-        possibleMoveEast = [piece.position[0]+1, piece.position[1]+1]
-        for (let i=0; i< allPositions.length; i+=1){
-            if(possibleMoveEast[0]>7 || possibleMoveEast[0]<0|| possibleMoveEast[1] > 7 || possibleMoveEast[1]<0){
-                answer = "Impossible"
-                    break;
-                }
-            else if(allPositions[i][0] === possibleMoveEast[0] && allPositions[i][1] === possibleMoveEast[1]){
-                answer = "Impossible"
-                break;
-        }
-           else {
-                answer = "Possible"
-            }
-        }return answer
-    }
-    else{
-        possibleMoveEast = [piece.position[0]+1, piece.position[1]-1]
-        for (let i=0; i< allPositions.length; i+=1){
-            if(possibleMoveEast[0]>7 || possibleMoveEast[0]<0|| possibleMoveEast[1] > 7 || possibleMoveEast[1]<0){
+        if(possibleMoveWest[0]>7 || possibleMoveWest[0]<0|| possibleMoveWest[1] > 7 || possibleMoveWest[1]<0){
             answer = "Impossible"
-                break;
-            }
-            else if(allPositions[i][0] === possibleMoveEast[0] && allPositions[i][1] === possibleMoveEast[1]){
-                answer = "Impossible"
-                break;
         }
-           else {
-                answer = "Possible"
-            }
-        }return answer
+        else if(arrAllPositions.indexOf(possibleMoveWest) === -1){
+            document.querySelector(`#y-${possibleMoveWest[1].toString()} > .x-${possibleMoveWest[0].toString()}`).classList+=' possible'
+            return possibleMoveWest
+        }
     }
 }
-//when a piece is touched movewest and moveEast are called, the possible moves will turn blur
-function jumpWest(piece, allPositions){
+
+function moveEast(piece, blackPieces, redPieces){
+    let answer = ''
+    let arrBlackPositions = [];
+    let arrRedPositions = [];
+    for (let b=0; b< blackPieces.length; b+=1){
+        arrBlackPositions += blackPieces[b].position
+    };
+    for (let r=0; r< redPieces.length; r+=1){
+        arrRedPositions += redPieces[r].position
+    }
+    arrAllPositions = arrBlackPositions.concat(arrRedPositions)
     if (piece.team === 'black'){
-        possibleMoveWest = [piece.position[0]-1, piece.position[1]+1];
-        for (let i=0; i< allPositions.length; i+=1){
-            if(allPositions[i][0] === possibleMoveWest[0] && allPositions[i][1] === possibleMoveWest[1]){
-                for (let j = 0; j<= allPositions.length; j+=1){
-                    possibleJumpWest = [piece.position[0]-2, piece.position[1]+2]
-                    if(allPositions[j][0] === possibleJumpWest[0] && allPositions[j][1] === possibleJumpWest[1]){
-                        break
-                    }
-                    else{
-                        return "possible jump"
-                    }
-                }
-            }
+        possiblemoveEast = [piece.position[0]+1, piece.position[1]+1];
+        if(possiblemoveEast[0]>7 || possiblemoveEast[0]<0 || possiblemoveEast[1] > 7 || possiblemoveEast[1]<0){
+        answer = 'impossible'
+        }
+        else if(arrAllPositions.indexOf(possiblemoveEast) === -1){
+            document.querySelector(`#y-${possiblemoveEast[1].toString()} > .x-${possiblemoveEast[0].toString()}`).classList+=' possible'
+            return possiblemoveEast
         }
     }
     else {
-        possibleMoveWest = [piece.position[0]-1, piece.position[1]-1];
-        for (let i=0; i< allPositions.length; i+=1){
-            if(allPositions[i][0] === possibleMoveWest[0] && allPositions[i][1] === possibleMoveWest[1]){
-                for (let j = 0; j<= allPositions.length; j+=1){
-                    possibleJumpWest = [piece.position[0]-2, piece.position[1]-2]
-                    if(allPositions[j][0] === possibleJumpWest[0] && allPositions[j][1] === possibleJumpWest[1]){
-                        break
-                    }
-                    else{
-                        return "possible jump"
-                    }
-                }
-            }
+        possiblemoveEast = [piece.position[0]+1, piece.position[1]-1]
+        if(possiblemoveEast[0]>7 || possiblemoveEast[0]<0|| possiblemoveEast[1] > 7 || possiblemoveEast[1]<0){
+            answer = "Impossible"
+        }
+        else if(arrAllPositions.indexOf(possiblemoveEast) === -1){
+            document.querySelector(`#y-${possiblemoveEast[1].toString()} > .x-${possiblemoveEast[0].toString()}`).classList+=' possible'
+            return possiblemoveEast
         }
     }
 }
-
-function jumpEast(piece, allPositions){
+function jumpWeast(piece, blackPieces, redPieces){
+    let answer = ''
+    let arrBlackPositions = [];
+    let arrRedPositions = [];
+    for (let b=0; b< blackPieces.length; b+=1){
+        arrBlackPositions += blackPieces[b].position
+    };
+    for (let r=0; r< redPieces.length; r+=1){
+        arrRedPositions += redPieces[r].position
+    }
+    arrAllPositions = arrBlackPositions.concat(arrRedPositions)
     if (piece.team === 'black'){
-        possibleMoveEast = [piece.position[0]+1, piece.position[1]+1];
-        for (let i=0; i< allPositions.length; i+=1){
-            if(allPositions[i][0] === possibleMoveEast[0] && allPositions[i][1] === possibleMoveEast[1]){
-                for (let j = 0; j<= allPositions.length; j+=1){
-                    possibleJumpEast = [piece.position[0]+2, piece.position[1]+2]
-                    if(allPositions[j][0] === possibleJumpEast[0] && allPositions[j][1] === possibleJumpEast[1]){
-                        break
-                    }
-                    else{
-                        return "possible jump"
-                    }
-                }
-            }
+        possibleJumpWeast = [piece.position[0]-1, piece.position[1]+1];
+        possibleLandingJumpWeast = [piece.position[0]-2, piece.position[1]+2];
+        if(possibleLandingJumpWeast[0]>7 || possibleLandingJumpWeast[0]<0 || possibleLandingJumpWeast[1] > 7 || possibleLandingJumpWeast[1]<0){
+        answer = 'impossible'
+        }
+        else if(arrRedPositions.indexOf(possibleJumpWeast) > -1 && arrAllPositions.indexOf(possibleLandingJumpWeast) === -1){
+            document.querySelector(`#y-${possibleLandingJumpWeast[1].toString()} > .x-${possibleLandingJumpWeast[0].toString()}`).classList+=' possibleJump'
+            return possibleLandingJumpWeast
         }
     }
     else {
-        possibleMoveEast = [piece.position[0]+1, piece.position[1]-1];
-        for (let i=0; i< allPositions.length; i+=1){
-            if(allPositions[i][0] === possibleMoveEast[0] && allPositions[i][1] === possibleMoveEast[1]){
-                for (let j = 0; j<= allPositions.length; j+=1){
-                    possibleJumpEast = [piece.position[0]+2, piece.position[1]-2]
-                    if(allPositions[j][0] === possibleJumpEast[0] && allPositions[j][1] === possibleJumpEast[1]){
-                        break
-                    }
-                    else{
-                        return "possible jump"
-                    }
-                }
+        possibleJumpWeast = [piece.position[0]-1, piece.position[1]-1];
+        possibleLandingJumpWeast = [piece.position[0]-2, piece.position[1]-2];
+        if(possibleLandingJumpWeast[0]>7 || possibleLandingJumpWeast[0]<0 || possibleLandingJumpWeast[1] > 7 || possibleLandingJumpWeast[1]<0){
+            answer = 'impossible'
             }
+            else if(arrBlackPositions.indexOf(possibleJumpWeast) > -1 && arrAllPositions.indexOf(possibleLandingJumpWeast) === -1){
+             document.querySelector(`#y-${possibleLandingJumpWeast[1].toString()} > .x-${possibleLandingJumpWeast[0].toString()}`).classList+=' possibleJump'
+            return possibleLandingJumpWeast
         }
     }
 }
 
-// function Jump(piece, allPositions)
-//     returns possibleJumps[]
+function jumpEast(piece, blackPieces, redPieces){
+    let answer = ''
+    let arrBlackPositions = [];
+    let arrRedPositions = [];
+    for (let b=0; b< blackPieces.length; b+=1){
+        arrBlackPositions += blackPieces[b].position
+    };
+    for (let r=0; r< redPieces.length; r+=1){
+        arrRedPositions += redPieces[r].position
+    }
+    arrAllPositions = arrBlackPositions.concat(arrRedPositions)
+    if (piece.team === 'black'){
+        possibleJumpEast = [piece.position[0]-1, piece.position[1]+1];
+        possibleLandingJumpEast = [piece.position[0]-2, piece.position[1]+2];
+        if(possibleLandingJumpEast[0]>7 || possibleLandingJumpEast[0]<0 || possibleLandingJumpEast[1] > 7 || possibleLandingJumpEast[1]<0){
+        answer = 'impossible'
+        }
+        else if(arrRedPositions.indexOf(possibleJumpEast) > -1 && arrAllPositions.indexOf(possibleLandingJumpEast) === -1){
+            document.querySelector(`#y-${possibleLandingJumpEast[1].toString()} > .x-${possibleLandingJumpEast[0].toString()}`).classList+=' possibleJump'
+            return possibleLandingJumpEast
+        }
+    }
+    else {
+        possibleJumpEast = [piece.position[0]-1, piece.position[1]-1];
+        possibleLandingJumpEast = [piece.position[0]-2, piece.position[1]-2];
+        if(possibleLandingJumpEast[0]>7 || possibleLandingJumpEast[0]<0 || possibleLandingJumpEast[1] > 7 || possibleLandingJumpEast[1]<0){
+            answer = 'impossible'
+            }
+            else if(arrBlackPositions.indexOf(possibleJumpEast) > -1 && arrAllPositions.indexOf(possibleLandingJumpEast) === -1){
+             document.querySelector(`#y-${possibleLandingJumpEast[1].toString()} > .x-${possibleLandingJumpEast[0].toString()}`).classList+=' possibleJump'
+            return possibleLandingJumpEast
+        }
+    }
+}
+/*colocar pecas
 
-black = createBlackPieces()
-red = createRedPieces()
-positionsRed = updatedPositions(red)
-positionsBlack = updatedPositions(black)
-allPos=allPositions(positionsBlack, positionsRed)
-// console.log(allPos)
-// console.log([black[0].position[0]+1, black[0].position[1]-1])
-// console.log(allPos[1][1])
-// console.log(black[4])
-
-console.log(moveWest(black[0],allPos))
-console.log(moveWest(black[1],allPos))
-console.log(moveWest(black[2],allPos))
-console.log(moveWest(black[3],allPos))
-console.log(moveWest(black[4],allPos))
-console.log(moveWest(black[5],allPos))
-console.log(moveWest(black[6],allPos))
-console.log(moveWest(black[7],allPos))
-console.log(moveWest(black[8],allPos))
-console.log(moveWest(black[9],allPos))
-console.log(moveWest(black[10],allPos))
-console.log(moveWest(black[11],allPos))
-// console.log(moveWest(red[0],allPos))
-// console.log(moveWest(red[1],allPos))
-// console.log(moveWest(red[2],allPos))
-// console.log(moveWest(red[3],allPos))
-// console.log(moveWest(red[4],allPos))
-// console.log(moveWest(red[5],allPos))
-// console.log(moveWest(red[6],allPos))
-// console.log(moveWest(red[7],allPos))
-// console.log(moveWest(red[8],allPos))
-// console.log(moveWest(red[9],allPos))
-// console.log(moveWest(red[10],allPos))
-// console.log(moveWest(red[11],allPos))
-// console.log(moveEast(black[0],allPos))
-// console.log(moveEast(black[1],allPos))
-// console.log(moveEast(black[2],allPos))
-// console.log(moveEast(black[3],allPos))
-// console.log(moveEast(black[4],allPos))
-// console.log(moveEast(black[5],allPos))
-// console.log(moveEast(black[6],allPos))
-// console.log(moveEast(black[7],allPos))
-// console.log(moveEast(black[8],allPos))
-// console.log(moveEast(black[9],allPos))
-// console.log(moveEast(black[10],allPos))
-// console.log(moveEast(black[11],allPos))
-// console.log(moveEast(red[0],allPos))
-// console.log(moveEast(red[1],allPos))
-// console.log(moveEast(red[2],allPos))
-// console.log(moveEast(red[3],allPos))
-// console.log(moveEast(red[4],allPos))
-// console.log(moveEast(red[5],allPos))
-// console.log(moveEast(red[6],allPos))
-// console.log(moveEast(red[7],allPos))
-// console.log(moveEast(red[8],allPos))
-// console.log(moveEast(red[9],allPos))
-// console.log(moveEast(red[10],allPos))
-// console.log(moveEast(red[11],allPos))
+tem algum pulo possivel?
+    se, sim 
+    quantos sao?
+        se 1, indicar para o jogador qual o pulo obrigatorio(voltar perguntar se tem um pulo possivel)
+        se >1, indicar para o jogador quais os pulos possiveis (obrigado a escolher um) (voltar perguntar se tem um pulo possivel)
+    se nao
+    esperar o jogador escolher uma peca
+    mostrar passos possiveis
+vez do oponente
 
 
+*/
